df272202a5925a463248433b47cfc0f3e2b3547d
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2012-10-07 02:42:13 +0800

    Add apt aliases and update other aliases

---
+++ home/.bash.d/apt.sh
@@ -0,0 +1,7 @@
+# I am lazy to type these all the times
+alias aptu='sudo apt-get -y update'
+alias aptdu='sudo apt-get -y dist-upgrade'
+alias apti='sudo apt-get -y install'
+alias aptr='sudo apt-get -y remove'
+alias aptar='sudo apt-get -y auto-remove'
+alias apts='apt-cache search'
