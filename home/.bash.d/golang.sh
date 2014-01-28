2f0d83361fba724b3dc4a02e67a024c43a3d242f
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-01-29 01:12:40 +0800

    Remove unused golang variables

--- home/.bash.d/golang.sh
+++ home/.bash.d/golang.sh
@@ -1,3 +1,12 @@
-export GOROOT=/usr/lib/go
-export GOBIN=/usr/bin/go
+# The GOPATH environment variable specifies the location of your workspace. It is
+# likely the only environment variable you'll need to set when developing Go
+# code.
+
+# To get started, create a workspace directory and set GOPATH accordingly. Your
+# workspace can be located wherever you like, but we'll use $HOME/go in this
+# document. Note that this must not be the sameath as your Go installation.
+# 
+# http://golang.org/doc/code.html
+export GOPATH=$HOME/Project/go
+
 alias gofmt='gofmt -tabs=false -tabwidth=4 -w'
