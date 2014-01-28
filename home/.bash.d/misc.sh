eb1838f2379bb29cc025ee03654f7b6f073e9780
Kian-Meng, Ang <kianmeng.ang@gmail.com>
2014-01-29 01:16:22 +0800

    Remove unused alias

--- home/.bash.d/misc.sh
+++ home/.bash.d/misc.sh
@@ -1,15 +1,2 @@
 alias xclip='xclip -selection c'
 
-# @see http://stevelosh.com/projects/t/
-alias s='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list someday'
-alias h='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list home'
-alias e='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list errands'
-alias p='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list phone'
-alias o='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list office'
-alias b='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list buy'
-
-# ongoing projects
-alias thesis='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list thesis'
-alias abbt='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list abbt'
-alias lpthw='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list lpthw'
-alias gplus='python ~/bin/t.py --task-dir ~/Dropbox/Tasks --list gplus'
