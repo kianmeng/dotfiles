529c2ab5983f466670014b6af7597fcc8ea07f50
Kian-Meng, Ang <kianmeng.ang@gmail.com>
2014-03-02 15:44:03 +0800

    Add alias for pushing to octopress' source branch

--- home/.bash.d/git.sh
+++ home/.bash.d/git.sh
@@ -1,3 +1,5 @@
 alias gitsm='git status -s | grep -v ??'
 alias gpom='git push origin master'
+# for pushing to octopress' source branch
+alias gpos='git push origin source'
 alias gitdiff='git diff --color'
