71c77b102efda5ca63e5c2e5194586b12d904038
Kian-Meng, Ang <kianmeng.ang@gmail.com>
2014-03-02 16:40:38 +0800

    Add ackg alias since I always confuse the ack (kanji code converter) with ack-grep (text finder)

--- home/.bash.d/git.sh
+++ home/.bash.d/git.sh
@@ -1,5 +1,8 @@
-alias gitsm='git status -s | grep -v ??'
+# typical git usage
 alias gpom='git push origin master'
+alias gitcm='git commit -m'
+alias gitsm='git status -s | grep -v ??'
+alias gitdiff='git diff --color'
+
 # for pushing to octopress' source branch
 alias gpos='git push origin source'
-alias gitdiff='git diff --color'
--- home/.bash.d/misc.sh
+++ home/.bash.d/misc.sh
@@ -1,2 +1,5 @@
 alias xclip='xclip -selection c'
 
+# since ack is kanji code converter and ack-grep is the actual program for
+# searching source code
+alias ackg='ack-grep'
