d3588698dc58f1495838f7126d24a86ac3ec5b57
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-03-16 19:12:16 +0800

    Autoload python modules during shell startup

--- .bash.d/python.sh
+++ .bash.d/python.sh
@@ -1,3 +1,12 @@
+# autoload certain essential modules
+# pip install pprint
+# pip install awesome_print
+# pip install ipython
+#
+# http://docs.python.org/2/using/cmdline.html#envvar-PYTHONSTARTUP
+# see
+export PYTHONSTARTUP=$HOME/.pythonstartup
+
 # remove all compiled python code recursively
 alias rmpyc='find . -type f -name "*.pyc" -delete'
 
