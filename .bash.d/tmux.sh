ef6575a4bab1ccf65c934747a865476d207899d5
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-06-07 23:50:50 +0800

    Add tmuxinator alias
    Auto install all the necessary packages when you run the command for the
    first time.

--- .bash.d/tmux.sh
+++ .bash.d/tmux.sh
@@ -7,3 +7,34 @@
 if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
     tmux attach || tmux new; exit
 fi
+
+# wrapper for tmuxinator but with additional installation checking for fresh
+# new installation.
+#
+function tmuxinator(){
+    RUBY_GEM_BIN=$(command -v `which gem`)
+    if [ ! -n "$RUBY_GEM_BIN" ] 
+    then
+        echo "rubygems missing. Installing rubygems..."
+        sudo apt-get install rubygems
+    fi
+
+    TMUXINATOR_BIN=$(command -v `which tmuxinator`)
+    if [ ! -n "$TMUXINATOR_BIN" ] 
+    then
+        echo "tmuxinator missing. Installing tmuxinator gem..."
+        sudo gem install tmuxinator
+
+        echo "Checking everything is okay..."
+        `which tmuxinator` doctor
+        return
+    fi
+
+    `which tmuxinator` "$@"
+}
+
+# another wrapper for the alias mux so we can check for installation.
+#
+function mux() {
+    tmuxinator "$@"
+}
