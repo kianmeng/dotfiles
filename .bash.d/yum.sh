b4cc0789321e43260d6c7996bbc83de121be65c3
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-09-21 08:50:35 -0400

    Add yum common aliases

---
+++ .bash.d/yum.sh
@@ -0,0 +1,26 @@
+# similar to apt.sh, some aliases to reduce typing.
+
+# general stuff
+alias yumsh='sudo yum shell'
+alias yumhist='sudo yum history'
+alias yumu='sudo yum update'
+alias yumcu='sudo yum check-update'
+
+# common actions
+# alias may conflict with YUMI (Your Universal Multibot Integrator) tool
+alias yumi='sudo yum install'
+alias yumri='sudo yum reinstall'
+alias yumdown='sudo yum downgrade'
+alias yumr='sudo yum remove'
+alias yums='sudo yum search'
+alias yumif='sudo yum info'
+alias yumfs='sudo yum provides'
+
+# group-related
+alias yumgi='sudo yum groupinstall'
+alias yumgr='sudo yum groupremove'
+alias yumgu='sudo yum groupupdate'
+alias yumgl='sudo yum grouplist'
+
+# repo-related
+alias yumrepo='sudo yum repolist all'
