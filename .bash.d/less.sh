33a9851abc87ec59122aeff1ac6f7c304c578ac5
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-09-21 08:34:00 -0400

    Fix incorrect path to source-highlight shell script
    This check the path of the shell script and export the LESSOPEN
    environment variable accordingly.

--- .bash.d/less.sh
+++ .bash.d/less.sh
@@ -3,7 +3,15 @@
 # source code highlighting
 # $ apt-get install source-highlight
 # see http://linux-tips.org/article/78/syntax-highlighting-in-less
-export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+HILITE_SH=$(command -v src-hilite-lesspipe.sh)
+if [ -n "$HILITE_SH" ] 
+then
+    # fedora
+    export LESSOPEN="| $HILITE_SH %s"
+else
+    # debian/ubuntu
+    export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+fi
 
 # -rR option is tell less to interpret those color codes and escape sequences
 # see http://unix.stackexchange.com/questions/19317/can-less-retain-colored-output
