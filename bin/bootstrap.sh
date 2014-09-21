221d6f89e54f0aa0b2d1daa8fbb0fc540fd3eefa
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-09-21 07:46:02 -0400

    Make bootstrap script support Fedora-20
    Detect and update the required RPM packages.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,23 +3,35 @@
 # Bootstrap the dotfiles installation
 
 echo -e "Install essential packages...\n"
-sudo apt-get install git git-sh tig meld exuberant-ctags xclip mercurial vim \
-    tmux screen source-highlight apt-file terminator ack-grep ipython ncdu pydf \
-    dstat htop speedometer aria2
+
+if [[ $(python -mplatform | grep -i fedora) ]];
+then
+    sudo yum install git tig meld ctags xclip mercurial vim \
+        tmux screen source-highlight terminator dstat htop aria2 \
+	aria2 python-virtualenv python-virtualenvwrapper \
+        python-flake8 glances subversion
+fi
+
+if [[ $(python -mplatform | grep -i ubuntu) ]];
+then
+    sudo apt-get install git git-sh tig meld exuberant-ctags xclip mercurial vim \
+        tmux screen source-highlight apt-file terminator ack-grep ipython ncdu pydf \
+        dstat htop speedometer aria2 subversion
+
+    echo -e "Update Deb packaing details...\n"
+    sudo apt-file update
+
+    echo "Update font cache...\n"
+    fc-cache -vf ~/.fonts
+
+    echo -e "Install essential Python modules...\n"
+    sudo pip install awesome_print virtualenvwrapper flake8 Glances
+fi
 
 echo "Clone the dotfiles repository and create soft links...\n"
 git clone https://github.com/kianmeng/dotfiles.git
 cd dotfiles
 ./bin/dfm install
-
-echo -e "Update Deb packaing details...\n"
-sudo apt-file update
-
-echo "Update font cache...\n"
-fc-cache -vf ~/.fonts
-
-echo -e "Install essential Python modules...\n"
-sudo pip install awesome_print virtualenvwrapper flake8 Glances
 
 echo -e "Install all Vim's plugins...\n"
 vim +BundleInstall +qall
