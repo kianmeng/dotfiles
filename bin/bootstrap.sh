8e8a57dedf653f63f88ae269002a30c8e4aad03e
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2014-09-21 07:09:41 -0400

    Fix echo not displaying new line

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,7 +2,7 @@
 #
 # Bootstrap the dotfiles installation
 
-echo "Install essential packages...\n"
+echo -e "Install essential packages...\n"
 sudo apt-get install git git-sh tig meld exuberant-ctags xclip mercurial vim \
     tmux screen source-highlight apt-file terminator ack-grep ipython ncdu pydf \
     dstat htop speedometer aria2
@@ -12,16 +12,16 @@ git clone https://github.com/kianmeng/dotfiles.git
 cd dotfiles
 ./bin/dfm install
 
-echo "Update Deb packaing details...\n"
+echo -e "Update Deb packaing details...\n"
 sudo apt-file update
 
 echo "Update font cache...\n"
 fc-cache -vf ~/.fonts
 
-echo "Install essential Python modules...\n"
+echo -e "Install essential Python modules...\n"
 sudo pip install awesome_print virtualenvwrapper flake8 Glances
 
-echo "Install all Vim's plugins...\n"
+echo -e "Install all Vim's plugins...\n"
 vim +BundleInstall +qall
 
-echo "Done! Enjoy!\n"
+echo -e "Done! Enjoy!\n"
