5521f884ad3dda5b4191df14d4fca4294c686074
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-02-01 19:51:31 +0800

    Add find and search bash function
    Quick Bash's function to search and replace text from current directory
    recursively.

--- .bash.d/filedir.sh
+++ .bash.d/filedir.sh
@@ -11,3 +11,13 @@ alias ls='ls --color --group-directories-first -p -CF';
 alias ll='ls -lh --color';
 alias lsa='ls -ahCF';
 alias lla='ls -alh';
+
+# search and replace
+# @see http://www.reddit.com/r/programming/comments/2tj6d6/findrep_easy_tool_to_find_and_replace/cnzy34o
+function findreplace { 
+    if [ ! $# == 2 ]; then 
+        echo "Usage: find_replace <search_string> <replace_string>"
+        return -1
+    fi
+    find . -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
+}
