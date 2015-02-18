bf5eafa00260b0c0c61444aaaeeef42705d4c980
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-02-18 08:51:18 +0800

    Add process related Bash aliases

---
+++ .bash.d/ps.sh
@@ -0,0 +1,15 @@
+# show process listing with tree
+alias ps='ps auxf'
+
+# search prcess using grep
+# @see https://www.digitalocean.com/community/tutorials/an-introduction-to-useful-bash-aliases-and-functions
+alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+# kill process forcefully
+alias fkill='kill -9'
+
+# kill process by port number
+# @see http://unix.stackexchange.com/a/92721
+function killport() { 
+    sudo kill -9 $(sudo fuser -n tcp $1 2> /dev/null);
+}
