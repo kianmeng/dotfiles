eb7a1ff73caa7f3e96cc57f11149519d81e112b5
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-02-20 13:25:05 +0800

    Update Nim & Nimble path and installation steps
    Move everything related to Nim into its own nim.sh file so we only need
    to update one place.

--- .bash.d/nim.sh
+++ .bash.d/nim.sh
@@ -1,9 +1,35 @@
-# Nim programming language related aliases. 
+# Nim programming language related aliases and environment variables
+# Installation steps from http://howistart.org/posts/nim/1
+
+# Nim binary path
+# See http://nim-lang.org/
+#
+# $ git clone https://github.com/Araq/Nim
+# $ cd Nim
+# $ git clone --depth 1 https://github.com/nim-lang/csources
+# $ cd csources && sh build.sh
+# $ cd ..
+# $ bin/nim c koch
+# $ ./koch boot -d:release
+if [[ ! "$PATH" =~ "$HOME/Projects/Nim/bin" ]]; then
+    export PATH=$PATH:$HOME/Projects/Nim/bin
+fi
+
+# Nimble binary path
+# See https://github.com/nim-lang/nimble#readme
+#
+# $ git clone https://github.com/nim-lang/nimble
+# $ cd nimble
+# $ nim c -r src/nimble install
+# $ nimble update
+if [[ ! "$PATH" =~ "$HOME/.nimble/bin" ]]; then
+    export PATH=$PATH:$HOME/.nimble/bin
+fi
 
 # c - compile, r - run, different verbosity.
 alias nimc='nim c -r --verbosity:0'
-alias nimcv='nim c -r --verbosity:1'
-alias nimcvv='nim c -r --verbosity:2'
+alias nimcv='nim c -r --verbosity:1' # this is the default
+alias nimcvv='nim c -r --verbosity:2' # this will show the line of source with issue
 alias nimcvvv='nim c -r --verbosity:3'
 
 # compile a release version which turn off checking and turn on optimization.
