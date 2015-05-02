d37c14fd6eb270e117c5d70d57b88c8d4dc95e80
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-05-03 06:24:46 +0800

    Install tlp packages during bootstrap.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,7 +11,7 @@ then
 	    aria2 python-virtualenv python-virtualenvwrapper \
         python-flake8 glances subversion sensible-utils most pass \
         google-noto-sans-simplified-chinese-fonts google-noto-sans-traditional-chinese-fonts \
-        shutter
+        shutter powertop tlp tlp-rdw
 fi
 
 if [[ $(python -mplatform | grep -i ubuntu) ]];
@@ -20,7 +20,7 @@ then
         tmux screen source-highlight apt-file terminator ack-grep ipython ncdu pydf \
         dstat htop speedometer aria2 subversion most i3-wm i3status i3lock \
         fcitx fcitx-sunpinyin ttf-dejavu fonts-droid fonts-wqy-microhei fonts-cantarell \
-        shutter
+        shutter powertop tlp tlp-rdw
 
     echo -e "Update Deb packaing details...\n"
     sudo apt-file update
