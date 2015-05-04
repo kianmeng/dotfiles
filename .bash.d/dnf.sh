2ef777a339c9baebb42372ac05b4e421f9c33fa6
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-05-04 22:27:43 +0800

    Categorized DNF-related aliases

--- .bash.d/dnf.sh
+++ .bash.d/dnf.sh
@@ -1,15 +1,18 @@
-# dnf
+# common
 alias dnfi='sudo dnf install -y'
 alias dnfu='sudo dnf update'
 alias dnfug='sudo dnf upgrade'
-alias dnfri='sudo dnf reinstall -y'
+alias dnfs='sudo dnf search'
+alias dnfp='sudo dnf provides'
 alias dnfr='sudo dnf erase'
 alias dnfif='sudo dnf info'
-alias dnfs='sudo dnf search'
-alias dnffs='sudo dnf provides'
+
+# optional
+alias dnfri='sudo dnf reinstall -y'
 alias dnfh='sudo dnf history'
 alias dnfhi='sudo dnf history info'
 
+# similar to rpm -qa
 alias dnfl='sudo dnf list'
 alias dnfli='sudo dnf list installed'
 alias dnfla='sudo dnf list available'
