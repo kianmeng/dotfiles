94bb289f7c28ece39c44f193be636c3beec36316
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-05-08 05:58:09 +0800

    Install the silver searcher during bootstrap.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -11,7 +11,7 @@ then
 	    aria2 python-virtualenv python-virtualenvwrapper \
         python-flake8 glances subversion sensible-utils most pass \
         google-noto-sans-simplified-chinese-fonts google-noto-sans-traditional-chinese-fonts \
-        shutter powertop tlp tlp-rdw
+        shutter powertop tlp tlp-rdw the_silver_searcher
 fi
 
 if [[ $(python -mplatform | grep -i ubuntu) ]];
@@ -20,7 +20,7 @@ then
         tmux screen source-highlight apt-file terminator ack-grep ipython ncdu pydf \
         dstat htop speedometer aria2 subversion most i3-wm i3status i3lock \
         fcitx fcitx-sunpinyin ttf-dejavu fonts-droid fonts-wqy-microhei fonts-cantarell \
-        shutter powertop tlp tlp-rdw
+        shutter powertop tlp tlp-rdw silversearcher-ag
 
     echo -e "Update Deb packaing details...\n"
     sudo apt-file update
