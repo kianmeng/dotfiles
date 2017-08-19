43ebc798bdc0fe0b1cbcb8fbe99433f9c97feb78
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2017-08-19 23:42:38 +0800

    Add LXC/LXD aliases and functions

--- .bash.d/lxd.sh
+++ .bash.d/lxd.sh
@@ -1,6 +1,6 @@
 # Is better to use this to trace down issue as lxc and lxd sometimes just
 # freeze there.
-function lxcd() {
+function lxdd() {
     if [[ $# -eq 0 ]]; then
         lxc
     else
@@ -9,22 +9,12 @@ function lxcd() {
 }
 
 # Enter the LXC container and launch Bash shell.
-function lxcsh() {
+function lxdsh() {
     if [[ $# -eq 0 ]]; then
         echo "ERR: Missing container name. Cannot launch Bash Shell!"
     else
-        lxcd exec $1 /bin/bash
+        lxdd exec $1 /bin/sh
     fi
 }
 
-alias lxcls='lxc-ls --fancy'
-alias lxclsr='lxcls --running'
-alias lxclsa='lxcls --active'
-alias lxclss='lxcls --stopped'
-
-alias lxcst='lxc-start -n'
-alias lxckill='lxc-stop -k -n'
-alias lxcreboot='lxc-stop -r -n'
-
-alias lxclog='lxc-console -n'
-alias lxcsh='lxc-attach -n'
+alias lxdls='lxc list'
