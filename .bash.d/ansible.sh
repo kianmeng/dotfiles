467f312d2db9bce91ef3bb35d3c765964ade270d
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2018-06-19 00:43:15 +0800

    Even more purging of unused sh files

--- .bash.d/ansible.sh
+++
@@ -1,4 +0,0 @@
-# no need to specify hosts file everytime we run the command
-# see http://terraltech.com/configuring-and-managing-computers-with-ansible/
-alias ansible_local='ansible -c local localhost'
-alias ansible_local_ping='ansible_local -m ping'
