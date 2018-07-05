0b348ec5779936407ee1feaef8ee70cadb2ea29a
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2018-07-05 23:01:39 +0800

    Fix terminal cannot start due to missing tmux

--- .bash.d/tmux.sh
+++ .bash.d/tmux.sh
@@ -2,12 +2,17 @@
 # shell start.
 [[ $- != *i* ]] && return
 
-# for tmux: export 256color
+# Do no proceed when tmux is not installed.
+if ! [ -x "$(command -v tmux)" ]; then
+    return
+fi
+
+# For tmux: export 256color.
 # @see https://wiki.archlinux.org/index.php/Tmux#Setting_the_correct_term
 [ -n "$TMUX" ] && export TERM=screen-256color
 
-# start tmux on every shell login. add this before aliases declaration
-# http://bionicraptor.co/2011/07/24/how-to-automatically-launch-tmux/
+# Start tmux on every shell login. add this before aliases declaration.
+# @see http://bionicraptor.co/2011/07/24/how-to-automatically-launch-tmux/
 if [ $TERM != "screen-256color" ] && [  $TERM != "screen" ]; then
     tmux attach || tmux new; exit
 fi
